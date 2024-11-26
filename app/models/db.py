760573ec5d98028205eef667899b02b25620a599
JuanT20 <79216153+JuanT20@users.noreply.github.com>
2024-11-26 17:02:24 -0500

    Update Files

--- app/models/db.py
+++ app/models/db.py
@@ -10,7 +10,7 @@ def get_connection():
         host='127.0.0.1',
         user='root',
         password='',
-        database='torneo',
+        database='kickoff',
         port=3306
         )
         return conecxion
